36c0d24b89b1ad12184f234559cca0f14af4e9fc
Shihab Mahamud <shihab4t@gmail.com>
2023-04-06 00:22:42 +0600

    Refactor the leaderboard page component

--- src/pages/Leaderboard.jsx
+++ src/pages/Leaderboard.jsx
@@ -7,6 +7,88 @@ import { selectUser } from "../features/auth/authSelectors";
 import { useGetQuizMarksQuery } from "../features/quizMark/quizMarkApi";
 import { errorTost } from "../utils/commonUtil";
 
+const getStudentsFormAssignmentMarks = assignmentMarks => {
+    let students = [];
+    assignmentMarks.forEach(a => {
+        if (a.status === "published") {
+            const studentIndex = students.findIndex(s => s.id === a.student_id);
+            if (studentIndex === -1) {
+                students.push({
+                    id: a.student_id,
+                    name: a.student_name,
+                    assignmentMark: Number(a.mark),
+                    quizMark: 0,
+                    rank: -1,
+                });
+            } else {
+                students[studentIndex] = {
+                    ...students[studentIndex],
+                    assignmentMark: students[studentIndex].assignmentMark + Number(a.mark),
+                };
+            }
+        }
+    });
+    return students;
+};
+
+const mutateStudentsWithQuizMarks = (students, quizMarks) => {
+    quizMarks.forEach(q => {
+        const studentIndex = students.findIndex(s => s.id === q.student_id);
+        if (studentIndex === -1) {
+            students.push({
+                id: q.student_id,
+                name: q.student_name,
+                assignmentMark: 0,
+                quizMark: Number(q.totalMark),
+                rank: -1,
+            });
+        } else {
+            students[studentIndex] = {
+                ...students[studentIndex],
+                quizMark: students[studentIndex].quizMark + Number(q.totalMark),
+            };
+        }
+    });
+    return students;
+};
+
+const sortStudentsByTotalMarks = students => {
+    return students.sort((a, b) => {
+        const totalMarksA = a.assignmentMark + a.quizMark;
+        const totalMarksB = b.assignmentMark + b.quizMark;
+        return totalMarksB - totalMarksA;
+    });
+};
+
+const mutateStudentsWithProperRank = students => {
+    let lastTotalMark = Infinity;
+    let lastRank = 0;
+
+    return students.map(student => {
+        const totalMark = student.assignmentMark + student.quizMark;
+        if (totalMark !== lastTotalMark) {
+            lastRank++;
+        }
+        lastTotalMark = totalMark;
+        return { ...student, rank: lastRank };
+    });
+};
+
+const getLoggedStudent = (students, userId) => {
+    const you = students.find(s => s.id === userId);
+    if (you) {
+        return you;
+    } else {
+        return {
+            id: loggedUser?.id,
+            name: loggedUser?.name,
+            assignmentMark: 0,
+            quizMark: 0,
+            rank: -1,
+        };
+    }
+};
+
 const Leaderboard = () => {
     const loggedUser = selectUser();
 
@@ -40,77 +122,14 @@ const Leaderboard = () => {
             assignmentMarks.length !== 0 &&
             quizMarks.length !== 0
         ) {
-            let students = [];
-            assignmentMarks.forEach(a => {
-                if (a.status === "published") {
-                    const studentIndex = students.findIndex(s => s.id === a.student_id);
-                    if (studentIndex === -1) {
-                        students.push({
-                            id: a.student_id,
-                            name: a.student_name,
-                            assignmentMark: Number(a.mark),
-                            quizMark: 0,
-                            rank: -1,
-                        });
-                    } else {
-                        students[studentIndex] = {
-                            ...students[studentIndex],
-                            assignmentMark: students[studentIndex].assignmentMark + Number(a.mark),
-                        };
-                    }
-                }
-            });
-            quizMarks.forEach(q => {
-                const studentIndex = students.findIndex(s => s.id === q.student_id);
-                if (studentIndex === -1) {
-                    students.push({
-                        id: q.student_id,
-                        name: q.student_name,
-                        assignmentMark: 0,
-                        quizMark: Number(q.totalMark),
-                        rank: -1,
-                    });
-                } else {
-                    students[studentIndex] = {
-                        ...students[studentIndex],
-                        quizMark: students[studentIndex].quizMark + Number(q.totalMark),
-                    };
-                }
-            });
+            let students = getStudentsFormAssignmentMarks(assignmentMarks);
+            students = mutateStudentsWithQuizMarks(students, quizMarks);
+            students = sortStudentsByTotalMarks(students);
+            students = mutateStudentsWithProperRank(students);
 
-            students = students.sort((a, b) => {
-                const totalMarksA = a.assignmentMark + a.quizMark;
-                const totalMarksB = b.assignmentMark + b.quizMark;
-                return totalMarksB - totalMarksA;
-            });
-
-            let lastTotalMark = Infinity;
-            let lastRank = 0;
-
-            students = students.map(student => {
-                const totalMark = student.assignmentMark + student.quizMark;
-                if (totalMark !== lastTotalMark) {
-                    lastRank++;
-                }
-                lastTotalMark = totalMark;
-                return { ...student, rank: lastRank };
-            });
-
-            const you = students.find(s => s.id === loggedUser?.id);
-            if (you) {
-                setYou(you);
-            } else {
-                setYou({
-                    id: loggedUser?.id,
-                    name: loggedUser?.name,
-                    assignmentMark: 0,
-                    quizMark: 0,
-                    rank: -1,
-                });
-            }
+            setYou(getLoggedStudent(students, loggedUser?.id));
 
             students = students.filter(student => student.rank <= 20);
-
             setStudents(students);
         }
     }, [assignmentMarks, quizMarks]);
